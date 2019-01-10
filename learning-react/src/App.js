94cfac07746e046df8fe2168d262b18b415e75f7
Vijeth <vijeth.n@position2.com>
2019-01-10 16:19:33 +0530

    Added files

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// state :
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       name: 'Vj',
+//       age: 25
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.name}</h1>
+//         <h2>{this.state.age}</h2>
+//       </div>
+//     );
+//   }
+// }
+
+// changing State: 
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      name: 'Vj',
-      age: 25
+      count: 0
     }
+    this.handleClick = this.handleClick.bind(this);
   }
+  handleClick() {
+    // Changing the state
+    // this.setState({ count: 10 })
 
+    // Checking Previous state and adding the count
+    this.setState(prevState => {
+      return {
+        count: prevState.count+1
+      }
+    })
+  }
   render() {
-    return (
+    return(
       <div>
-        <h1>{this.state.name}</h1>
-        <h2>{this.state.age}</h2>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
       </div>
-    );
+    )
   }
 }
-
-
 export default App;
